8e7c46cd673eb94438cc4edcfe0e03e479497932
Trikest <salmo.00000850311@unicap.br>
2025-04-21 12:04:49 -0300

    Chatbot

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,13 @@ import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
 import Services from "../components/Services";
 import Footer from "../components/Footer";
-
+import Chatbot from "../components/Chatbot";
 function Home() {
   return (
     <div className="home-container">
       <Banner />
       <Services />
+      <Chatbot />
     </div>
   );
 }
