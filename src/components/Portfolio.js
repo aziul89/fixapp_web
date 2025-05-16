70d363550665072fee100ee8d4f28ae541d486a5
Maria Luiza Farias <130606221+aziul89@users.noreply.github.com>
2025-05-16 23:24:21 +0000

    serviços

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,10 +11,10 @@ const responsive = {
 
 // ADD portfólio da empresa (esses são exemplos)
 const portfolio = [
-  { img: "https://via.placeholder.com/300", title: "Carro Protegido", description: "Instalação de película premium para maior conforto térmico." },
-  { img: "https://via.placeholder.com/300", title: "Vidros Residenciais", description: "Aplicação de película refletiva para privacidade." },
-  { img: "https://via.placeholder.com/300", title: "Fachada Empresarial", description: "Película fumê para reduzir o calor e melhorar a estética." },
-  { img: "https://images.unsplash.com/photo-1551961760-c635a013c046?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGFyZWRlJTIwZGVzZW5ob3xlbnwwfHwwfHx8MA%3D%3D", title: "Decoração com Película", description: "Personalização de vidros com estilo e sofisticação." }
+  { img: "https://img.freepik.com/fotos-premium/trabalhador-de-servico-automovel-aplicando-nano-revestimento-em-um-detalhe-do-carro_179755-10517.jpg?ga=GA1.1.1617786885.1714433104&semt=ais_hybrid&w=740", title: "Sinalização de Veículos", description: "Transforme seu veículo em uma poderosa ferramenta de divulgação. Aplicamos adesivos personalizados com alta durabilidade e acabamento profissional, garantindo visibilidade da sua marca onde quer que vá." },
+  { img: "https://img.freepik.com/fotos-premium/cartao-de-nome-startup-company-cartao-de-visita-energetic-orange-color-plastico-bussines-ideia-de-conceito_1020495-51871.jpg?ga=GA1.1.1617786885.1714433104&semt=ais_hybrid&w=740", title: "Placas em Acrílico", description: "Produzimos placas elegantes e resistentes em acrílico, ideais para empresas, consultórios, sinalizações internas e externas. Com acabamento refinado, nossas placas conferem sofisticação e profissionalismo ao seu espaço." },
+  { img: "https://img.freepik.com/fotos-gratis/balcao-de-cozinha-moderno-de-renderizacao-3d-com-design-branco-e-biege_105762-2228.jpg?ga=GA1.1.1617786885.1714433104&semt=ais_hybrid&w=740", title: "Envelopamento de Móveis", description: "Renove seus móveis sem precisar comprar novos. O envelopamento é uma solução prática e econômica para dar um novo visual a mesas, armários, balcões e muito mais — com opções de texturas, cores e efeitos modernos." },
+  { img: "https://img.freepik.com/fotos-premium/tingimento-do-carro-trabalhador-aplicando-folha-de-tingimento-na-janela-do-carro_473712-4471.jpg?ga=GA1.1.1617786885.1714433104&semt=ais_hybrid&w=740", title: "Aplicação de Película Fumê", description: "Oferecemos instalação de películas fumês para residências, comércios e veículos. Elas reduzem o calor, garantem mais privacidade e ainda valorizam o ambiente com um toque de sofisticação e mais elegância." }
 ];
 
 function PortfolioCarousel() {
