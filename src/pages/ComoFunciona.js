45a876ed213d18408b6527742ccd62b9e08b0044
Maria Luiza Farias <130606221+aziul89@users.noreply.github.com>
2025-06-02 15:50:28 +0000

    update pag como funciona

--- src/pages/ComoFunciona.js
+++ src/pages/ComoFunciona.js
@@ -15,10 +15,10 @@ function ComoFunciona() {
         <h2>Como funciona?</h2>
         <ul className="passos-lista">
           <li><span>✅</span> <strong>Crie sua conta:</strong> Em poucos cliques, você se cadastra e tem acesso completo à plataforma.</li>
-          <li><span>✅</span> <strong>Escolha o serviço:</strong> Encontre o tipo de serviço ideal para sua necessidade: envelopamento de móveis, placas personalidades, película fumê e muito mais.</li>
+          <li><span>✅</span> <strong>Escolha o serviço:</strong> Encontre o tipo de serviço ideal para sua necessidade: envelopamento de móveis, placas personalizadas, película fumê e muito mais.</li>
           <li><span>✅</span> <strong>Personalize o pedido:</strong> Preencha um formulário rápido com as especificações do serviço: metragem, tipo de material, local de aplicação, etc.</li>
           <li><span>✅</span> <strong>Pague com segurança:</strong> Finalize com pagamento via cartão de crédito, débito ou Pix.</li>
-          <li><span>✅</span> <strong>Conte com o FixBot:</strong> Nosso assistente virtual tira todas as suas dúvidas em tempo real!</li>
+          <li><span>✅</span> <strong>Conte com o SuporteFix:</strong> Nossos atendentes tiram todas as suas dúvidas em tempo real!</li>
         </ul>
 
         <p className="final-texto">
