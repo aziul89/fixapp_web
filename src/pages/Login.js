205af5ceca905a5019518738146da87bd703f8f4
Trikest <salmo.00000850311@unicap.br>
2025-06-15 11:54:20 -0300

    Login ajuste

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
     }
 
     try {
-      const response = await fetch("https://ideiafix-back-end-1test.onrender.com/api/login", {
+      const response = await fetch("http://localhost:3000/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, senha })
